3043f12cf0d3cfd340a10e41a6068535dbe9e490
mangomaza <asadmaza@outlook.com>
2026-04-26 20:23:51 +0800

    bugfix: missing auth blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,6 @@ def create_app(config_name='default'):
     from app.routes.main_routes import main_bp
     from app.routes.auth_routes import auth_bp
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
 
     return app
