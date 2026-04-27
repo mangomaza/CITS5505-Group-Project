5f79cfedd56e0bcd383f03dcf5d6c4a1e9f0b340
mangomaza <asadmaza@outlook.com>
2026-04-27 20:45:10 +0800

    set up database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from config import config
+from app.extensions import db, migrate
 
 
 def create_app(config_name='default'):
@@ -9,6 +10,11 @@ def create_app(config_name='default'):
                 static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'))
 
     app.config.from_object(config[config_name])
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes.main_routes import main_bp
     from app.routes.auth_routes import auth_bp
