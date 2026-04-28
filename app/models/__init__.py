aab021285d9b395c2cad8df917ffc8d0fe9481dc
mangomaza <asadmaza@outlook.com>
2026-04-28 21:57:01 +0800

    bugfix: import ingredient model so its table is detected

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.recipe import Recipe
+from app.models.ingredient import Ingredient
 from app.models.rating import Rating
 
-__all__ = ['User', 'Recipe', 'Rating']
+__all__ = ['User', 'Recipe', 'Ingredient', 'Rating']
