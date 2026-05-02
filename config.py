4fa43d7c7d5febc8b157245c778de3d0e1bb2528
mangomaza <asadmaza@outlook.com>
2026-05-02 16:26:32 +0800

    fix auth: removed fallback option for secret key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-me'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'instance', 'mixitup.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
